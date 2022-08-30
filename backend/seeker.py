f3a8f329a954c3081e8a2469635e3635a7e1f450
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-08-30 16:46:26 -0500

    feat: added backend files

---
+++ backend/seeker.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+from bs4 import BeautifulSoup
+
+def access(url, driver=None):
+    # print("Starting...")
+    options = Options()
+    options.add_argument("--headless")
+
+    if driver is None:
+        driver = webdriver.Edge(options=options)
+    
+    driver.get(url)    
+    driver.implicitly_wait(time_to_wait=5)
+    # print("Ready!")
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    return soup, driver
+
+
+def closer(driver):
+    driver.close()
