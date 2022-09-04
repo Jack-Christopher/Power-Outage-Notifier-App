b4f8144ebd814493c693f854abeb0c7c202e6124
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-09-04 11:51:28 -0500

    chore: changed server port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const reportsRouter = require('./routes/reports')
 
 // start the server
 app.listen(process.env.PORT || 5000, () => {
-    console.log('API server listening on port ' + port);
+    // console.log('API server listening on port ' + port);
     db.init();
 })
 
