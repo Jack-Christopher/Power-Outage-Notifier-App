57280ee6e206f31f979b4bf6fa64ffdc0a434e93
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-09-05 18:45:16 -0500

    fix: ECONNREFUSED of postgres

--- backend/services/db.js
+++ backend/services/db.js
@@ -8,64 +8,93 @@ const db = new Client({
   }
 });
 
+console.log('DATABASE_URL: ', process.env.DATABASE_URL);
+
 
 function init() {
-    // db.prepare(`CREATE TABLE IF NOT EXISTS settings (
-    //     key TEXT PRIMARY KEY,
-    //     value TEXT
-    // )`).run();
     db.connect();
     db.query(`CREATE TABLE IF NOT EXISTS settings (
         key TEXT PRIMARY KEY,
         value TEXT
-    )`);
+    )`, (err, res) => {
+        if (err) {
+            console.log('Error creating table: ', err);
+        } else {
+            console.log('Table created');
+        }
+    });
 
-    // insert default settings
-    // db.prepare(`INSERT OR IGNORE INTO settings (key, value) VALUES (?, ?)`).run('last_update', '2000-01-01');
-    db.query(`INSERT INTO settings (key, value) VALUES ($1, $2) ON CONFLICT (key) DO NOTHING`, ['last_update', '2000-01-01']);
+    db.query(`INSERT INTO settings (key, value) VALUES ($1, $2) ON CONFLICT (key) DO NOTHING`, ['last_update', '2000-01-01'], (err, res) => {
+        if (err) {
+            console.log('Error inserting into table: ', err);
+        } else {
+            console.log('Inserted into table');
+        }
+    });
 
-    // db.prepare(`CREATE TABLE IF NOT EXISTS reports (
-    //     id INTEGER PRIMARY KEY,
-    //     date TEXT,
-    //     description TEXT
-    // )`).run();
     db.query(`CREATE TABLE IF NOT EXISTS reports (
         id INTEGER PRIMARY KEY,
         date TEXT,
         description TEXT
-    )`);
+    )`, (err, res) => {
+        if (err) {
+            console.log('Error creating table: ', err);
+        } else {
+            console.log('Table created');
+        }
+    });
     db.end();
 }
 
 function getSetting(key) {
     db.connect();
-    // const row = db.prepare(`SELECT value FROM settings WHERE key = ?`).get(key);
-    // return row ? row.value : null;
-    const row = db.query(`SELECT value FROM settings WHERE key = $1`, [key]);
+    const row = db.query(`SELECT value FROM settings WHERE key = $1`, [key], (err, res) => {
+        if (err) {
+            console.log('Error getting setting: ', err);
+        } else {
+            console.log('Got setting');
+        }
+    });
     db.end();
     return row ? row.value : null;
 }
 
 function setSetting(key, value) {
     db.connect();
-    // db.prepare(`UPDATE settings SET value = ? WHERE key = ?`).run(value, key);
-    db.query(`UPDATE settings SET value = $1 WHERE key = $2`, [value, key]);
+    db.query(`UPDATE settings SET value = $1 WHERE key = $2`, [value, key], (err, res) => {
+        if (err) {
+            console.log('Error setting setting: ', err);
+        } else {
+            console.log('Set setting');
+        }
+    });
     db.end();
 }
 
 
 function addReport(report) {
     db.connect();
-//   db.prepare(`INSERT OR IGNORE INTO reports (id, date, description) VALUES (?, ?, ?)`).run(report.id, report.date, report.description);
-    db.query(`INSERT INTO reports (id, date, description) VALUES ($1, $2, $3) ON CONFLICT (id) DO NOTHING`, [report.id, report.date, report.description]);
+    db.query(`INSERT INTO reports (id, date, description) VALUES ($1, $2, $3) ON CONFLICT (id) DO NOTHING`, 
+        [report.id, report.date, report.description], (err, res) => {
+            if (err) {
+                console.log('Error inserting into table: ', err);
+            } else {
+                console.log('Inserted into table');
+            }
+        }
+    );
     db.end();
 }
 
 function getReports() {
     db.connect();
-    // const data = db.prepare(`SELECT * FROM reports ORDER BY date DESC`).all();
-    // return data;
-    const data = db.query(`SELECT * FROM reports ORDER BY date DESC`);
+    const data = db.query(`SELECT * FROM reports ORDER BY date DESC`, (err, res) => {
+        if (err) {
+            console.log('Error getting reports: ', err);
+        } else {
+            console.log('Got reports');
+        }
+    });
     db.end();
     return data;
 }
@@ -74,8 +103,13 @@ function getReports() {
 function getIdList() {
     var id_list = [];
     db.connect();
-    // const data = db.prepare(`SELECT id FROM reports ORDER BY date DESC`).all();
-    const data = db.query(`SELECT id FROM reports ORDER BY date DESC`);
+    const data = db.query(`SELECT id FROM reports ORDER BY date DESC`, (err, res) => {
+        if (err) {
+            console.log('Error getting reports: ', err);
+        } else {
+            console.log('Got reports');
+        }
+    });
     db.end();
     for (var i = 0; i < data.length; i++) {
         id_list.push(parseInt(data[i].id));
@@ -86,17 +120,32 @@ function getIdList() {
 
 function clear() {
     db.connect();
-    // db.prepare(`DELETE FROM reports`).run();
-    // db.prepare(`DELETE FROM settings`).run();
-    db.query(`DELETE FROM reports`);
-    db.query(`DELETE FROM settings`);
+    db.query(`DELETE FROM reports`, (err, res) => {
+        if (err) {
+            console.log('Error clearing table: ', err);
+        } else {
+            console.log('Cleared table');
+        }
+    });
+    db.query(`DELETE FROM settings`, (err, res) => {
+        if (err) {
+            console.log('Error clearing table: ', err);
+        } else {
+            console.log('Cleared table');
+        }
+    });
     db.end();
 }
 
 function query(sql, params) {
     db.connect();
-//   return dñb.prepare(sql).all(params).run();
-    var query = db.query(sql, params);
+    var query = db.query(sql, params, (err, res) => {
+        if (err) {
+            console.log('Error querying: ', err);
+        } else {
+            console.log('Queried');
+        }
+    });
     db.end();
     return query;
 
