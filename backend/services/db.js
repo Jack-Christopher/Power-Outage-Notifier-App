fd1d462f198ca0b171e195d191aec07e6f6fe2fb
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-09-05 18:35:17 -0500

    fix: ECONNREFUSED of postgres

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,14 +1,12 @@
 
-const { Pool } = require('pg')
+const { Client } = require('pg');
 
-// using env variables
-const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-})
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 
 function init() {
@@ -16,6 +14,7 @@ function init() {
     //     key TEXT PRIMARY KEY,
     //     value TEXT
     // )`).run();
+    db.connect();
     db.query(`CREATE TABLE IF NOT EXISTS settings (
         key TEXT PRIMARY KEY,
         value TEXT
@@ -35,38 +34,49 @@ function init() {
         date TEXT,
         description TEXT
     )`);
+    db.end();
 }
 
 function getSetting(key) {
+    db.connect();
     // const row = db.prepare(`SELECT value FROM settings WHERE key = ?`).get(key);
     // return row ? row.value : null;
     const row = db.query(`SELECT value FROM settings WHERE key = $1`, [key]);
+    db.end();
     return row ? row.value : null;
 }
 
 function setSetting(key, value) {
+    db.connect();
     // db.prepare(`UPDATE settings SET value = ? WHERE key = ?`).run(value, key);
     db.query(`UPDATE settings SET value = $1 WHERE key = $2`, [value, key]);
+    db.end();
 }
 
 
 function addReport(report) {
+    db.connect();
 //   db.prepare(`INSERT OR IGNORE INTO reports (id, date, description) VALUES (?, ?, ?)`).run(report.id, report.date, report.description);
     db.query(`INSERT INTO reports (id, date, description) VALUES ($1, $2, $3) ON CONFLICT (id) DO NOTHING`, [report.id, report.date, report.description]);
+    db.end();
 }
 
 function getReports() {
+    db.connect();
     // const data = db.prepare(`SELECT * FROM reports ORDER BY date DESC`).all();
     // return data;
     const data = db.query(`SELECT * FROM reports ORDER BY date DESC`);
+    db.end();
     return data;
 }
 
 
 function getIdList() {
     var id_list = [];
+    db.connect();
     // const data = db.prepare(`SELECT id FROM reports ORDER BY date DESC`).all();
     const data = db.query(`SELECT id FROM reports ORDER BY date DESC`);
+    db.end();
     for (var i = 0; i < data.length; i++) {
         id_list.push(parseInt(data[i].id));
     }
@@ -75,15 +85,21 @@ function getIdList() {
 }
 
 function clear() {
+    db.connect();
     // db.prepare(`DELETE FROM reports`).run();
     // db.prepare(`DELETE FROM settings`).run();
     db.query(`DELETE FROM reports`);
     db.query(`DELETE FROM settings`);
+    db.end();
 }
 
 function query(sql, params) {
-//   return db.prepare(sql).all(params).run();
-    return db.query(sql, params);
+    db.connect();
+//   return dñb.prepare(sql).all(params).run();
+    var query = db.query(sql, params);
+    db.end();
+    return query;
+
 }
 
 
