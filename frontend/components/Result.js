a8bf397b687682380504fea3892ba4711fcc08a0
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-09-07 16:48:45 -0500

    feat: component for rendering report list

--- frontend/components/Result.js
+++ frontend/components/Result.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import { GetStyles } from '../styles/GetStyles.js';
-import { Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 class Result extends Component {
 
     constructor(props)
     {
         super(props);
-        this.styles = GetStyles(["container", "appTitle", "button", "icon", "message", "item", "list", "input"]);
+        this.styles = GetStyles(["container", "appTitle", "button", "icon", "message", "result", "list", "input"]);
     }
 
     render() {
-        return(
-            <View style={this.styles.container}>
-                <Text
-                    style={this.styles.item}
-                >
-                    {this.props.district}, 
+        if (this.props.data.length > 0) {
+            return (
+                <View style={this.styles.list}>
+                    <FlatList
+                        // style={this.styles.list}
+                        data={this.props.data}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity
+                                onPress={() => this.props.navigation.navigate('Details', {descr: item.description})}
+                            >
+                                <Text style={this.styles.result}>
+                                    {item.description}
+                                </Text>
+                            </TouchableOpacity>
+                        )}
+                        keyExtractor={item => item.id}
+                    />
+                </View>
+            );
+        }
+        else{
+            return (
+                <Text style = {this.styles.message} >
+                    No data yet.              
                 </Text>
-            </View>
-        )
+            )
+        }
     }
 }
 
