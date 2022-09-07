b1d6478745f60fc382684fb5b95fc7fa781fa707
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-09-07 16:49:18 -0500

    feat: added Details Screen

--- frontend/App.js
+++ frontend/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/HomeScreen';
 import DistrictSetter from './screens/DistrictSetterScreen';
 import Results from './screens/ResultsScreen';
+import Details from './screens/DetailsScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ function MyStack()
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="DistrictSetter" component={DistrictSetter} />
             <Stack.Screen name="Results" component={Results} />
+            <Stack.Screen name="Details" component={Details} />
         </Stack.Navigator>
     );
 }
